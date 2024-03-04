6d0bfc900ec14b9c6c4bedef358030d9939d713b
ericg2 <l.ittlelettere@gmail.com>
2024-03-03 19:48:43 -0500

    mount olive changes

---
+++ src/main/java/frc/robot/commands/auto/ShootOnlyCommand.java
@@ -0,0 +1,18 @@
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Robot;
+import frc.robot.commands.shooter.ShootCommand;
+
+import static frc.robot.Constants.TrapArm.ARM_MAX_ROTATION;
+
+public class ShootOnlyCommand extends SequentialCommandGroup {
+    public ShootOnlyCommand() {
+        super(
+                Robot.swerve.resetCommand(),
+                Commands.runOnce(() -> Robot.arm.setAnglePosition(ARM_MAX_ROTATION)),
+                new ShootCommand()
+        );
+    }
+}
